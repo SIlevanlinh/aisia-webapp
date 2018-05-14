2a660b0b61901937aaea42e91bd91cdfce41e1b9
Le Van Linh <levanlinh@earth.sint.co.jp>
2018-05-14 18:08:44 +0900

    Dislay images called from placeholder API

---
+++ src/components/Frame.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="frame">
+    <ul id="example-1">
+      <li v-for="(photo, index) in photos" v-bind:key="index">
+        <img :src="photo.thumbnailUrl" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {HTTP} from '../http-common';
+
+export default {
+  data() {
+    return {
+      photos: [],
+      errors: []
+    }
+  },
+
+  created() {
+    HTTP.get('photos')
+    .then(response => {
+      this.photos = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped> 
+
+</style>
---
+++ src/views/FrameView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="frame-view">
+    <Frame/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Frame from '@/components/Frame.vue'
+
+export default {
+  name: 'frame-view',
+  components: {
+    Frame
+  }
+}
+</script>
