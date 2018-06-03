693d645445f8698c816f2762b2dbe17b9c696c23
SIlevanlinh <levanlinhcube@gmail.com>
2018-06-03 11:04:01 +0900

    add stitsla template

--- src/modules/frames/index.vue
+++ src/modules/frames/index.vue
@@ -1,55 +1,57 @@
 <template>
-  <div class="frames jumbotron" >
+  <div class="frames jumbotron">
     <div class="container">
       <div class="row">
         <div class="col-sm">
-          <Frame class="frame-item" :frame="defaultFrame"/>
+          <Frame class="frame-item" :frame="defaultFrame" />
         </div>
         <div class="col-sm">
-          <FrameList class="frame-list" :frames="frames"/>
+          <FrameList class="frame-list" :frames="frames" />
         </div>
       </div>
-    </div>   
-  </div>  
+    </div>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import store from "./_store";
-import FrameList from "./_components/FrameList";
-import Frame from "./_components/Frame";
+  import {
+    mapGetters
+  } from "vuex";
+  import store from "./_store";
+  import FrameList from "./_components/FrameList";
+  import Frame from "./_components/Frame";
 
-export default {
-  name: "FramesIndex",
-  components: {
-    FrameList,
-    Frame
-  },
-  computed: {
-    ...mapGetters({
-      frames: "$_frames/frames",
-      defaultFrame: "$_frames/currentFrame"
-    })
-  },
-  created() {
-    const STORE_KEY = "$_frames";
-    if (!(STORE_KEY in this.$store._modules.root._children)) {
-      this.$store.registerModule(STORE_KEY, store);
+  export default {
+    name: "FramesIndex",
+    components: {
+      FrameList,
+      Frame
+    },
+    computed: {
+      ...mapGetters({
+        frames: "$_frames/frames",
+        defaultFrame: "$_frames/currentFrame"
+      })
+    },
+    created() {
+      const STORE_KEY = "$_frames";
+      if (!(STORE_KEY in this.$store._modules.root._children)) {
+        this.$store.registerModule(STORE_KEY, store);
+      }
+    },
+    mounted() {
+      this.$store.dispatch("$_frames/getFrames");
     }
-  },
-  mounted() {
-    this.$store.dispatch("$_frames/getFrames");
-  }
-};
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.frame-item {
-  float: left;
-}
+  .frame-item {
+    float: left;
+  }
 
-.frame-list {
-  float: right;
-}
+  .frame-list {
+    float: right;
+  }
 </style>
